cb92a577c033439bf65151d60873a889fbaf38af
gabriele <gabrielepalaj@hotmail.com>
2023-11-13 20:39:21 +0100

    Correzione moduli

---
+++ server-core/src/main/java/com/palaj/server/App.java
@@ -0,0 +1,13 @@
+package com.palaj.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ server-product/src/main/java/com/palaj/server/product/App.java
@@ -0,0 +1,13 @@
+package com.palaj.server.product;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
